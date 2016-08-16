5d7c33d05f1ab00b640d562b28d1da1219768d77
Soumya Ray <soumya.ray@gmail.com>
2016-08-16 13:52:44 +0800

    add print_paths method: print reduced path coefficient matrix

--- R/syntax.R
+++ R/syntax.R
@@ -111,9 +111,29 @@ modelr <- function(data, measurement_model, interactions=NULL, structural_model)
 
 
 # Report Functions
+print_paths <- function(model_estimated, na.print=".", digits=2) {
+  endogenous <- unique(model_estimated$model$strucmod[,"target"])
+  exogenous <- unique(model_estimated$model$strucmod[,"source"])
+  latent <- model_estimated$model$latent
+  structure_spec <- model.pls.estimated$model$D
 
-plot_scores <- function(fitted_model) {
-  plot(as.data.frame(fitted_model$factor_scores), pch = 16,
-       col = rgb(0.5, 0.5, 0.5, alpha = 0.6))
+  # create matrix of relevant path coefficients and NAs otherewise
+  path_matrix <- matrix(nrow = length(latent), ncol = length(latent), dimnames = list(latent, latent))
+  path_matrix[structure_spec == 1] <- model_estimated$path_coefficients[structure_spec == 1]
+
+  # add R Squared row
+  r_sq <- t(rSquared(model_estimated))[1, ]
+  path_matrix <- rbind(r_sq, path_matrix)
+  rownames(path_matrix) <- c("R^2", latent)
+
+  # round and print
+  final_paths <- round(path_matrix[c("R^2", exogenous), endogenous], digits)
+  print(final_paths, na.print = na.print)
 }
 
+plot_scores <- function(fitted_model, factors=NULL) {
+  if (missing(factors)) factors <- fitted_model$model$latent
+
+  plot(as.data.frame(fitted_model$factor_scores[, factors]), pch = 16,
+       col = rgb(0.5, 0.5, 0.5, alpha = 0.6))
+}
