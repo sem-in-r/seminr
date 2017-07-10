76b9fa6f727d816345b213baa28ff1b126e3e7bd
Nicholas <nicholasdanks@hotmail.com>
2017-07-10 11:49:16 +0800

    seminr and simplePLS work with R / C / F

--- R/warnings.R
+++ R/warnings.R
@@ -1,10 +1,10 @@
-#' @export
+# Warning to check for formative only constructs
 warning_only_formative_construct <- function(mmMatrix) {
   latents <- unique(mmMatrix[,1])
 
   for(latent in latents) {
-    if(length(items_per_mode(latent,"F",mmMatrix)) == nrow(mmMatrix[mmMatrix[,"latent"] == latent,])) {
-      cat(c("Warning:",latent,"is purely formatively defined\n"))
+    if(length(items_per_mode(latent,"F",mmMatrix)) == nrow(mmMatrix_per_latent(latent,mmMatrix))) {
+      cat(c("*** Warning:",latent,"is purely formatively defined. ***\n"))
     }
   }
 }
