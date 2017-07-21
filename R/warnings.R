64c6e1e6e59326aec0292116426676c15c966847
Nicholas <nicholasdanks@hotmail.com>
2017-07-21 16:47:57 +0800

    Added a warning and break for single item formative constructs

--- R/warnings.R
+++ R/warnings.R
@@ -4,7 +4,21 @@ warning_only_formative_construct <- function(mmMatrix) {
 
   for(latent in latents) {
     if(length(items_per_mode(latent,"F",mmMatrix)) == nrow(mmMatrix_per_latent(latent,mmMatrix))) {
-      cat(c("*** Warning:",latent,"is purely formatively defined. ***\n"))
+      warning(c(latent," is purely formatively defined.\n"))
     }
   }
+}
+
+warning_single_item_formative <- function(mmMatrix) {
+  ltVariables <- unique(mmMatrix[,1])
+  for(latent in ltVariables) {
+    if(nrow(mmMatrix_per_latent(latent,mmMatrix)) == 1 && mmMatrix_per_latent(latent,mmMatrix)[,3] == "F") {
+      stop("You cannot define a single item latent as formative")
+    }
+  }
+}
+
+warnings <- function(mmMatrix) {
+  warning_only_formative_construct(mmMatrix)
+  warning_single_item_formative(mmMatrix)
 }
