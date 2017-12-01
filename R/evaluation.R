a5c85712ca661de491471b3f2e6a1733f5964f76
NicholasDanks <nicholasdanks@iss.nthu.edu.tw>
2017-12-01 08:33:44 +0800

    added AVE metric

--- R/evaluation.R
+++ R/evaluation.R
@@ -13,23 +13,25 @@ SRMR <- function(seminr_model) {
 }
 #  End of not working -------------------
 
-# Dillon-Goldstein's rhoC
-rho_c <- function(seminr_model){
-  dgr <- matrix(NA, nrow=length(seminr_model$ltVariables), ncol=1)
+# RhoC and AVE
+reliability <- function(seminr_model){
+  dgr <- matrix(NA, nrow=length(seminr_model$ltVariables), ncol=2)
   rownames(dgr) <- seminr_model$ltVariables
-  colnames(dgr) <- c("rhoC")
+  colnames(dgr) <- c("rhoC", "AVE")
   for(i in seminr_model$ltVariables){
     if(measure_mode(i,seminr_model$mmMatrix)=="B"| measure_mode(i,seminr_model$mmMatrix)=="A"){
       x <- seminr_model$outer_loadings[, i]
       ind <- which(x!=0)
       if(length(ind)==1){
-        dgr[i,1] <- 1
+        dgr[i,1:2] <- 1
       } else {
        x <- x[ind]
        dgr[i,1] <- sum(x)^2 / (sum(x)^2 + sum(1-x^2))
+       dgr[i,2] <- sum(x^2)/length(x)
       }
     } else {
       dgr[i,1] <- N/A
+      dgr[i,2] <- sum(x^2)/length(x)
     }
   }
   return(dgr)
