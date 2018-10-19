ea38c27b7ee8bfd34cb4446f71b9794ec62c8ab4
Nicholas Danks <nicholasdanks@hotmail.com>
2018-10-19 10:22:06 +0800

    Version 0.5.3 (#88)
    * Made bootstrap reproducible.
    
    * Modified all expect_equal() tests to have a tolerance of 0.00001 for floating point operations on 32bit platforms. R CMD CHECK passes all tests pass.
    
    * Correcting for single item bug fix in Issue #84
    
    * Changed logic in PLSc
    
    * create demo for single item interactions
    
    * Adresses Issue #84 single item interaction bug
    
    * Coerce rhoA to 1 for all interactions for calculation of PLSc
    
    * bumped version and date.
    
    * Changed version number
    
    * Unsure about how to report reliabilities for consistent interactions.
    
    * Remove this demo
    
    * Added tests to address Issue #84 Single item interaction bug
    
    * Bumped version number, date and added cran-comments. Amended the vignette to reflect the change in allowing PLSc on interactions.

--- cran-comments.md
+++ cran-comments.md
@@ -1,21 +1,9 @@
 ## Resubmission
 This is a resubmission. In this version I have:
 
-## Attended to a CRAN error:
-We see such failures occasionally: for the record this time it is
-
- SEMinR Model succesfully bootstrapped── 1. Failure: Seminr evaluates the factor discriminant validity p_values correc
- diff[1, 2] is not strictly less than 0.1. Difference: 0.000314
-
-Seems you failed to heed the warnings in §1.6 of 'Writing R Extensions': please re-read it and correct ASAP (CRAN is shut until Sep 10) and before Sep 21 to safely retain the package on CRAN.
-
-Also try a spell checker --- it is 'successfully'.
-
--- 
-Brian D. Ripley,                  ripley@stats.ox.ac.uk
-Emeritus Professor of Applied Statistics, University of Oxford
-
-Specifically, we updated the bootstrap method to take a seed for reproducability and added a tolerance of 0.0001 for floating point calculation tests. All tests including random processes such as bootstrapping were made reproducible.
+## Attended to a user-reported error:
+Single-item interactions were generating a bug in the code. 
+Issue #84 on https://github.com/sem-in-r/seminr/issues/84
 
 ## Test environments
 * macOS Sierra 10.12.6 (on travis-ci), R 3.5.0
---
+++ tests/testthat/test-consistent-interactions.R
@@ -0,0 +1,53 @@
+context("SEMinR correctly estimates PLSc for a simple model with interaction\n")
+
+# Test cases
+## Simple case
+# Creating our measurement model
+mobi_mm <- constructs(
+  reflective("Image",        multi_items("IMAG", 1:5)),
+  reflective("Expectation",  single_item("CUEX3")),
+  reflective("Value",        multi_items("PERV", 1:2)),
+  reflective("Satisfaction", multi_items("CUSA", 1:3))
+)
+
+# Interaction constructs must be created after the measurement model is defined.
+# We are using the orthogonalization method as per Henseler & Chin (2010)
+mobi_xm <- interactions(
+  interaction_ortho("Image", "Expectation")
+)
+
+# Structural model
+#  note: interactions should be the names of its main constructs joined by a '*' in between.
+mobi_sm <- relationships(
+  paths(to = "Satisfaction",
+        from = c("Image", "Expectation", "Value",
+                 "Image*Expectation")
+  )
+)
+
+# Load data, assemble model, and estimate using simplePLS
+mobi_pls <- estimate_pls(data = mobi,
+                         measurement_model = mobi_mm,
+                         interactions = mobi_xm,
+                         structural_model = mobi_sm)
+# Load outputs
+sum <- summary(mobi_pls)
+
+## Output originally created using following lines
+# write.csv(sum$paths, file = "tests/fixtures/plsc-interaction-paths.csv")
+# write.csv(sum$reliability, file = "tests/fixtures/plsc-interaction-reliability.csv")
+
+# Load controls
+paths_control <- as.matrix(read.csv("../fixtures/plsc-interaction-paths.csv", row.names = 1))
+reliability_control <- as.matrix(read.csv("../fixtures/plsc-interaction-reliability.csv", row.names = 1))
+
+# Testing
+
+test_that("Seminr estimates paths and R2 correctly\n", {
+  expect_equal(sum$paths, paths_control, tolerance = 0.00001)
+})
+
+test_that("Seminr estimates rhoA, AVE, rhoC (reliability) correctly\n", {
+  expect_equal(sum$reliability, reliability_control, tolerance = 0.00001)
+})
+
