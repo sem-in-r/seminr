080f72e65127d8c14f374d355e6f21ae46154417
André Calero Valdez <andrecalerovaldez@gmail.com>
2020-12-17 15:11:38 +0100

    added first version of model viz

---
+++ tests/testthat/test-plot_model.R
@@ -0,0 +1,53 @@
+test_that("plot_model does not fail", {
+   mobi <- mobi
+
+   #seminr syntax for creating measurement model
+   mobi_mm <- constructs(
+                reflective("Image",        multi_items("IMAG", 1:5)),
+                reflective("Expectation",  multi_items("CUEX", 1:3)),
+                reflective("Quality",      multi_items("PERQ", 1:7)),
+                reflective("Value",        multi_items("PERV", 1:2)),
+                reflective("Satisfaction", multi_items("CUSA", 1:3)),
+                reflective("Complaints",   single_item("CUSCO")),
+                reflective("Loyalty",      multi_items("CUSL", 1:3))
+              )
+   #seminr syntax for creating structural model
+   mobi_sm <- relationships(
+     paths(from = "Image",        to = c("Expectation", "Satisfaction", "Loyalty")),
+     paths(from = "Expectation",  to = c("Quality", "Value", "Satisfaction")),
+     paths(from = "Quality",      to = c("Value", "Satisfaction")),
+     paths(from = "Value",        to = c("Satisfaction")),
+     paths(from = "Satisfaction", to = c("Complaints", "Loyalty")),
+     paths(from = "Complaints",   to = "Loyalty")
+   )
+
+   mobi_pls <- estimate_pls(data = mobi,
+                            measurement_model = mobi_mm,
+                            structural_model = mobi_sm)
+
+  res <-  plot_model(mobi_pls)
+  expect_equal(res, "result string")
+})
+
+
+test_that("model2", {
+  mobi <- mobi
+
+  #seminr syntax for creating measurement model
+  mobi_mm <- constructs(
+    reflective("Image",        multi_items("IMAG", 1:5)),
+    reflective("Expectation",  multi_items("CUEX", 1:3))
+  )
+  #seminr syntax for creating structural model
+  mobi_sm <- relationships(
+    paths(from = "Image",        to = c("Expectation"))
+  )
+
+  mobi_pls <- estimate_pls(data = mobi,
+                           measurement_model = mobi_mm,
+                           structural_model = mobi_sm)
+
+  res <-  plot_model(mobi_pls)
+  expect_equal(res, "result string")
+})
+
