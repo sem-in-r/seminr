e2cda9a66d47121c003495c395c41b512328aa37
André Calero Valdez <andrecalerovaldez@gmail.com>
2021-01-03 17:40:29 +0100

    added preliminary support for lavaan models using semPlot package.

--- R/graph_dot.R
+++ R/graph_dot.R
@@ -1,42 +1,5 @@
-# TEST
-if (FALSE) {
-  library(seminr)
-
-  mobi <- mobi
-
-  #seminr syntax for creating measurement model
-  mobi_mm <- constructs(reflective("Image",        multi_items("IMAG", 1:5)),
-                        reflective("Expectation",  multi_items("CUEX", 1:3)),
-                        reflective("Quality",      multi_items("PERQ", 1:7)),
-                        reflective("Value",        multi_items("PERV", 1:2)),
-                        reflective("Satisfaction", multi_items("CUSA", 1:3)),
-                        reflective("Complaints",   single_item("CUSCO")),
-                        composite("Loyalty",      multi_items("CUSL", 1:3)))
-
-  #seminr syntax for creating structural model
-  mobi_sm <- relationships(paths(from = "Image",        to = c("Expectation", "Satisfaction", "Loyalty")),
-                           paths(from = "Expectation",  to = c("Quality", "Value", "Satisfaction")),
-                           paths(from = "Quality",      to = c("Value", "Satisfaction")),
-                           paths(from = "Value",        to = c("Satisfaction")),
-                           paths(from = "Satisfaction", to = c("Complaints", "Loyalty")),
-                           paths(from = "Complaints",   to = "Loyalty"))
-
-  mobi_pls <- estimate_pls(data = mobi,
-                           measurement_model = mobi_mm,
-                           structural_model = mobi_sm)
-
-  plot_theme <- create_theme(plot.title.fontsize = 40,
-                             plot.fontname = "Times",
-                             mm.node.fill = "firebrick",
-                             sm.node.fill = "pink")
-
-  dot_graph(mobi_pls, theme = plot_theme) %>% DiagrammeR::grViz()
-}
-
-
+# To allow some dot notation here and there
 globalVariables(c("."))
-
-
 
 #' Plot various SEMinR models
 #'
@@ -57,11 +20,23 @@ plot_model <- function(model,
                        theme = NULL,
                        ...){
   if (requireNamespace("DiagrammeR", quietly = TRUE)) {
+    # lavaan models
+    if (inherits(model, "cfa_model")) {
+      message("Plotting of lavaan models using semPlot.")
+      dot_graph.cfa_model(model, ...)
+      return()
+    }
+    if (inherits(model, "cbsem_model")) {
+      message("Plotting of lavaan models using semPlot.")
+      dot_graph.cbsem_model(model, ...)
+      return()
+    }
+
     res <- DiagrammeR::grViz(dot_graph(model, title, theme, ...))
     set_last_seminr_plot(res)
     res
   } else {
-    message("This function requires the DiagrammeR package. You can install it by calling: install.packages(\"DiagrammeR\")")
+    stop("This function requires the DiagrammeR package. You can install it by calling: install.packages(\"DiagrammeR\")")
   }
 }
 
@@ -118,6 +93,10 @@ save_plot <- function(filename = "RPlot.pdf", plot = last_seminr_plot(), width =
   }
   if (!requireNamespace("rsvg", quietly = TRUE)) {
     stop("This function requires the rsvg package. You can install it by calling: install.packages(\"rsvg\")")
+  }
+
+  if (is.null(plot)) {
+    stop("No compatible plot was created.")
   }
 
 
@@ -216,6 +195,43 @@ dot_graph <- function(model,
   UseMethod("dot_graph")
 }
 
+#' Plotting of confirmatory factor analysis models using semPLOT
+#'
+#' For a full description of parameters see \link[semPlot]{semPaths}
+#'
+#' @param model the CFA model
+#' @param title Unused
+#' @param theme Unused
+#' @param what The metric to use for edges ("path", "est", "std", "eq", "col")
+#' @param whatLabels The metric to use for edge labels
+#' @param ... Parameters passed to the \link[semPlot]{semPaths} function
+#' @export
+dot_graph.cfa_model <- function(model, title = "", theme = NULL, what = "std", whatLabels = "std", ...){
+  if (!requireNamespace("semPlot", quietly = TRUE)) {
+    stop("Plotting models from lavaan is not implemented yet. As a fallback you can install \"semPlot\" and then utilized.")
+  }
+
+  semPlot::semPaths(model$lavaan_model, what = what, whatLabels = whatLabels,...)
+}
+
+#' Plotting of covariance based SEMs models using semPLOT
+#'
+#' For a full description of parameters see \link[semPlot]{semPaths}
+#'
+#' @param model the CBSEM model
+#' @param title Unused
+#' @param theme Unused
+#' @param what The metric to use for edges ("path", "est", "std", "eq", "col")
+#' @param whatLabels The metric to use for edge labels
+#' @param ... Parameters passed to the \link[semPlot]{semPaths} function
+#' @export
+dot_graph.cbsem_model <- function(model, title = "", theme = NULL, what = "std", whatLabels = "std", ...){
+  if (!requireNamespace("semPlot", quietly = TRUE)) {
+    stop("Plotting models from lavaan is not implemented yet. As a fallback you can install \"semPlot\" and then utilized.")
+  }
+
+semPlot::semPaths(model$lavaan_model, what = what, whatLabels = whatLabels,...)
+}
 
 dot_graph.default <- function(...){
   stop("Whoops. This shouldn't have happened. Did you use an unsupported model type? Please let us know if this happens and how.")
@@ -673,7 +689,7 @@ extract_sm_edges <- function(model, theme, weights = 1) {
     edge_label <- ""
     if (theme$sm.edge.label.show) {
       edge_label <- paste0(", label = < <B>", letter, " = ", coef, "</B>" , suffix, " >")
-      cat(edge_label)
+      #cat(edge_label)
     }
 
     # add the weight
