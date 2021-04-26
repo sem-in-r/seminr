1d6e9fa51c03ddfa07b2fb4a882ca21360b484be
NicholasDanks <nicholasdanks@iss.nthu.edu.tw>
2021-04-26 14:58:38 +0100

    final commit to primer prep2

--- demo/seminr-pls-influencer.R
+++ demo/seminr-pls-influencer.R
@@ -2,12 +2,12 @@ library(seminr)
 
 # Create the measurement model
 influencer_mm <- constructs(
-  composite("SIC",  multi_items("SIC", 1:7), weights = mode_B),
-  composite("PL",   multi_items("PL", 1:4)),
-  composite("PQ",   multi_items("PQ", 1:4)),
-  composite("PI",   multi_items("PI", 1:5)),
-  composite("WTP",  single_item("WTP")),
-  composite("PIC",  multi_items("PIC", 1:5)),
+  composite("SIC",  multi_items("sic_", 1:7), weights = mode_B),
+  composite("PL",   multi_items("pl_", 1:4)),
+  composite("PQ",   multi_items("pq_", 1:4)),
+  composite("PI",   multi_items("pi_", 1:5)),
+  composite("WTP",  single_item("wtp")),
+  composite("PIC",  multi_items("pic_", 1:5)),
   interaction_term("PQ", "PIC", method = two_stage)
 )
 
@@ -25,4 +25,3 @@ influencer_pls <- estimate_pls(data = influencer_data,
                                structural_model = influencer_sm)
 
 summary(influencer_pls)
-data(influencer_data)
