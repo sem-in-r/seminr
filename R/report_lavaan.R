abcb7d1fdc91d6e5914f3e6dc19df7f39b3bf1ca
Soumya Ray <soumya.ray@gmail.com>
2025-08-21 19:31:50 +0800

    fix: correctly relabel lavaan summary output column as p-value

--- R/report_lavaan.R
+++ R/report_lavaan.R
@@ -15,9 +15,9 @@ summarize_cb_measurement <- function(object, alpha=0.05) {
                                     model$item_names, model$construct_names)
   alpha_text <- alpha/2*100
   significance <- with(loadings_df,
-                       data.frame(est.std, se, pvalue, ci.lower, ci.upper))
+                       data.frame(est.std, se, z, pvalue, ci.lower, ci.upper))
   rownames(significance) <- with(loadings_df, paste(lhs, "->", rhs))
-  colnames(significance) <- c( "Std Estimate", "SE", "t-Value", paste(alpha_text, "% CI", sep = ""), paste((100-alpha_text), "% CI", sep = ""))
+  colnames(significance) <- c( "Std Estimate", "SE", "z-Value", "p-Value", paste(alpha_text, "% CI", sep = ""), paste((100-alpha_text), "% CI", sep = ""))
 
   # Get descriptives and correlations
   available_item_names <- intersect(names(object$data), model$item_names)
